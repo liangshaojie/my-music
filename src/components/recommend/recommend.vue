5e69abe7916c169922da9743322f38e780fd74a9
877942720@qq.com <lsj890305>
2017-08-03 20:52:11 +0800

    tab切换完毕

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="hello">
+        5555
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+
+    }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    .recommend
+        position: fixed
+</style>
